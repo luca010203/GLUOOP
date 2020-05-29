945b8d2069009b418536b300d9c2a36daa6532e3
Luca Ciappa <532732@student.glu.nl>
2020-05-29 21:52:21 +0200

    first commit

---
+++ opdracht 2/class.php
@@ -0,0 +1,15 @@
+<?
+class Person {
+  // Properties
+  public $name;
+  public $lastname;
+
+  // Methods
+  function set_name($name) {
+    $this->name = $name;
+  }
+  function get_name() {
+    return $this->name;
+  }
+}
+?>
